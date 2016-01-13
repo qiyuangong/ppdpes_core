302eb150d7d84459b5f5c98c51d5aec6888b6f6f
qiyuangong <qiyuangong@gmail.com>
2016-01-13 16:03:29 +0800

    修改各项测试的返回值。

--- anonymizer.py
+++ anonymizer.py
@@ -4,21 +4,37 @@ run semi_partition with given parameters
 
 # !/usr/bin/env python
 # coding=utf-8
-from algorithm.semi_partition import semi_partition
-from algorithm.semi_partition_missing import semi_partition_missing
-from algorithm.mondrian import mondrian
-from algorithm.clustering_based_k_anon import anon_k_member, anon_k_nn
-from algorithm.Separation_Gen import Separation_Gen
-from algorithm.PAA import PAA
-
-from algorithm.anatomize import anatomize
-from utils.read_adult_data import read_data as read_adult
-from utils.read_adult_data import read_tree as read_adult_tree
-from utils.read_informs_data import read_data as read_informs
-from utils.read_informs_data import read_tree as read_informs_tree
-from utils.read_musk_data import read_data as read_musk
-from utils.read_musk_data import read_tree as read_musk_tree
 import sys, copy, random, cProfile
+try:
+    from algorithm.semi_partition import semi_partition
+    from algorithm.semi_partition_missing import semi_partition_missing
+    from algorithm.mondrian import mondrian
+    from algorithm.clustering_based_k_anon import anon_k_member, anon_k_nn
+    from algorithm.Separation_Gen import Separation_Gen
+    from algorithm.PAA import PAA
+    from algorithm.anatomize import anatomize
+except ImportError:
+    from .algorithm.semi_partition import semi_partition
+    from .algorithm.semi_partition_missing import semi_partition_missing
+    from .algorithm.mondrian import mondrian
+    from .algorithm.clustering_based_k_anon import anon_k_member, anon_k_nn
+    from .algorithm.Separation_Gen import Separation_Gen
+    from .algorithm.PAA import PAA
+    from .algorithm.anatomize import anatomize
+try:
+    from utils.read_adult_data import read_data as read_adult
+    from utils.read_adult_data import read_tree as read_adult_tree
+    from utils.read_informs_data import read_data as read_informs
+    from utils.read_informs_data import read_tree as read_informs_tree
+    from utils.read_musk_data import read_data as read_musk
+    from utils.read_musk_data import read_tree as read_musk_tree
+except ImportError:
+    from .utils.read_adult_data import read_data as read_adult
+    from .utils.read_adult_data import read_tree as read_adult_tree
+    from .utils.read_informs_data import read_data as read_informs
+    from .utils.read_informs_data import read_tree as read_informs_tree
+    from .utils.read_musk_data import read_data as read_musk
+    from .utils.read_musk_data import read_tree as read_musk_tree
 
 __DEBUG = False
 DATA_SELECT = 'i'
@@ -30,9 +46,10 @@ def get_result_one(alg, att_trees, data, k=DEFAULT_K):
     "run semi_partition for one time, with k=10"
     print "K=%d" % k
     data_back = copy.deepcopy(data)
-    _, eval_result = alg(att_trees, data, k)
+    result, eval_result = alg(att_trees, data, k)
     print "NCP %0.2f" % eval_result[0] + "%"
     print "Running time %0.2f" % eval_result[1] + "seconds"
+    return (result, eval_result)
 
 
 def get_result_k(alg, att_trees, data):
@@ -42,8 +59,10 @@ def get_result_k(alg, att_trees, data):
     data_back = copy.deepcopy(data)
     all_ncp = []
     all_rtime = []
+    all_k = []
     # for k in range(5, 105, 5):
     for k in [2, 5, 10, 25, 50, 100]:
+        all_k.append(k)
         _, eval_result = alg(att_trees, data, k)
         data = copy.deepcopy(data_back)
         all_ncp.append(round(eval_result[0], 2))
@@ -56,6 +75,7 @@ def get_result_k(alg, att_trees, data):
     print "All NCP", all_ncp
     print "All Running time", all_rtime
     print '#' * 30
+    return [all_k, all_ncp, all_rtime]
 
 
 def get_result_dataset(alg, att_trees, data, k=DEFAULT_K, n=10, joint=5000):
@@ -95,6 +115,7 @@ def get_result_dataset(alg, att_trees, data, k=DEFAULT_K, n=10, joint=5000):
     print "All NCP", all_ncp
     print "All Running time", all_rtime
     print '#' * 30
+    return [datasets, all_ncp, all_rtime]
 
 
 def get_result_qi(alg, att_trees, data, k=DEFAULT_K):
@@ -118,6 +139,7 @@ def get_result_qi(alg, att_trees, data, k=DEFAULT_K):
     print "All NCP", all_ncp
     print "All Running time", all_rtime
     print '#' * 30
+    return [range(1, ls), all_ncp, all_rtime]
 
 
 def get_result_missing(alg, att_trees, data, k=DEFAULT_K, n=10):
@@ -250,15 +272,15 @@ def universe_anonymizer(argv):
     except:
         FLAG = ''
     if FLAG == 'k':
-        get_result_k(ALG, ATT_TREES, RAW_DATA)
+        return get_result_k(ALG, ATT_TREES, RAW_DATA)
     elif FLAG == 'qi':
-        get_result_qi(ALG, ATT_TREES, RAW_DATA)
+        return get_result_qi(ALG, ATT_TREES, RAW_DATA)
     elif FLAG == 'data':
-        get_result_dataset(ALG, ATT_TREES, RAW_DATA)
+        return get_result_dataset(ALG, ATT_TREES, RAW_DATA)
     elif FLAG == 'one':
         if LEN_ARGV > 3:
             k = int(sys.argv[4])
-            get_result_one(ALG, ATT_TREES, RAW_DATA, k)
+            return get_result_one(ALG, ATT_TREES, RAW_DATA, k)
         else:
             if __DEBUG:
                 cProfile.run('get_result_one(ALG, ATT_TREES, RAW_DATA)')
@@ -268,14 +290,13 @@ def universe_anonymizer(argv):
         if __DEBUG:
             cProfile.run('get_result_one(ALG, ATT_TREES, RAW_DATA)')
         else:
-            get_result_one(ALG, ATT_TREES, RAW_DATA)
+            return get_result_one(ALG, ATT_TREES, RAW_DATA)
     else:
         print "Usage: python anonymizer [a | i | m] [s | m | knn | kmember] [k | qi | data | one]"
         print "a: adult dataset, i: INFORMS dataset, m: musk dataset"
         print "[s: semi_partition, m: mondrian, knn: k-nnn, kmember: k-member]"
         print "K: varying k, qi: varying qi numbers, data: varying size of dataset, \
                 one: run only once"
-    # anonymized dataset is stored in result
     print "Finish Anonymization!!"
 
 
